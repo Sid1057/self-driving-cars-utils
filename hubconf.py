9318c2423aef93b95b6ab0736118a3ce113acb4e
Sid1057 <ivanov.dale@gmail.com>
2019-10-21 00:16:29 +0300

    improved

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,38 @@
 ependencies = ['torch']
 
-from .semantic.DeepSqueeze.models import DeepSqueeze as DeepSqueeze_
+#from .semantic.DeepSqueeze.models import DeepSqueeze as DeepSqueeze_
+from torchvision.models._utils import IntermediateLayerGetter
+import torchvision.models.resnet as resnet
+
+from torch.nn import functional as F
+
+from collections import OrderedDict
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+
+squeeze = torchvision.models.squeezenet.squeezenet1_1(pretrained=True).eval()
+
+class DeepSqueeze_(torch.nn.Module):
+    def __init__(self, num_classes):
+        super(DeepSqueeze, self).__init__()
+        backbone = squeeze.features
+
+        classifier = DeepLabHead(512, num_classes)
+
+        self.backbone = backbone
+        self.classifier = classifier
+        self.aux_classifier = None
+
+    def forward(self, x):
+        input_shape = x.shape[-2:]
+        features = self.backbone(x.half())
+
+        result = OrderedDict()
+#         x = features["out"]
+        x = features
+        x = self.classifier(x)
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+
+        return x
 
 # resnet18 is the name of entrypoint
 def DeepSqueeze(pretrained=False, **kwargs):
