bb5313fd45671fbccd2e3edbf97da762c3470fdb
Sid1057 <ivanov.dale@gmail.com>
2019-10-21 00:12:00 +0300

    improved

--- hubconf.py
+++ hubconf.py
@@ -7,6 +7,6 @@ def DeepSqueeze(pretrained=False, **kwargs):
     model = DeepSqueeze_(35)
 
     checkpoint = 'https://github.com/Sid1057/self-driving-cars-utils/blob/master/semantic/DeepSqueeze/DeepLab_v3_squeeze11_kitti_classes_iou_mean012_iou_max60.pth'
-    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False)
+    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False))
 
     return model
