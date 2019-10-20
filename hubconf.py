fb640f31e08e526e9053b1cface6a30bfd9c43f5
Sid1057 <ivanov.dale@gmail.com>
2019-10-21 00:08:33 +0300

    improved

--- hubconf.py
+++ hubconf.py
@@ -1,10 +1,12 @@
 ependencies = ['torch']
 
-from semantic.DeepSqueeze.models import DeepSqueeze as DeepSqueeze_
+from .semantic.DeepSqueeze.models import DeepSqueeze as DeepSqueeze_
 
 # resnet18 is the name of entrypoint
 def DeepSqueeze(pretrained=False, **kwargs):
-    model = DeepSqueeze(35)
-    model.load_state_dict(torch.load('semantic/DeepSqueeze/models/DeepLab_v3_squeeze11_kitti_classes_iou_mean012_iou_max60.pth'))
+    model = DeepSqueeze_(35)
+
+    checkpoint = 'https://github.com/Sid1057/self-driving-cars-utils/blob/master/semantic/DeepSqueeze/DeepLab_v3_squeeze11_kitti_classes_iou_mean012_iou_max60.pth'
+    model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=False)
 
     return model
